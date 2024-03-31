60074e46549a42b96fdc7c16915f46a79cc091db
DavidLarsen1 <145472134+DavidLarsen1@users.noreply.github.com>
2024-03-30 19:22:38 -0600

    final done

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -1,9 +1,57 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation1 World!");
+
+        List<Video> videosList = new List<Video>();
+
+        // Video 1
+        Video video1 = new Video("How to get to first base", "Hyrum Tolman", 480);
+
+        Comment video1Comment1 = new Comment("David", "This video is truly inspiring! I love how you taking about how to talking to her.");
+        Comment video1Comment2 = new Comment("MrBeast", "Your content always surprises me. I want to fly you out to Florida.");
+        Comment video1Comment3 = new Comment("Murmut", "Wonderful! I wish i had that skill.");
+
+        video1.ListComment(video1Comment1);
+        video1.ListComment(video1Comment2);
+        video1.ListComment(video1Comment3);
+
+        videosList.Add(video1);
+
+        // Video 2
+        Video video2 = new Video("Pickled herring in my soup", "Jack Dalton", 975);
+
+        Comment video2Comment1 = new Comment("Diego", "Wow what a chef");
+        Comment video2Comment2 = new Comment("Bill", "BowTies are cool");
+        Comment video2Comment3 = new Comment("Alisha", "I go hard to this song");
+
+        video2.ListComment(video2Comment1);
+        video2.ListComment(video2Comment2);
+        video2.ListComment(video2Comment3);
+
+        videosList.Add(video2);
+
+        // Video 3
+        Video video3 = new Video("Let's country dance", "Miriam Velasquez Soto", 650);
+
+        Comment video3Comment1 = new Comment("Anna", "Wow, you can really dances.");
+        Comment video3Comment2 = new Comment("Peter", "How do you do that.");
+        Comment video3Comment3 = new Comment("Ralph", "woof woof");
+
+        video3.ListComment(video3Comment1);
+        video3.ListComment(video3Comment2);
+        video3.ListComment(video3Comment3);
+
+        videosList.Add(video3);
+
+        foreach (Video video in videosList)
+        {
+            video.DisplayInfo();
+            Console.WriteLine();
+            Console.WriteLine();
+        }
     }
 }
--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -1,9 +1,63 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation2 World!");
+        // Order 1
+        Address address1 = new Address("Adams St 120", "Paris", "Lima", "FR");
+        Customer customer1 = new Customer("Frank H. Jones", address1);
+        List<Product> productsList1 = new List<Product>();
+
+        Product p1001 = new Product("Tv", "GC200", 1000.00, 1);
+        Product p1002 = new Product("Sticky note", "NB50", 3.50, 5);
+        Product p1003 = new Product("Pencil", "MP05", 1.99, 10);
+
+        productsList1.Add(p1001);
+        productsList1.Add(p1002);
+        productsList1.Add(p1003);
+
+        Order order1 = new Order(productsList1, customer1);
+
+        Console.WriteLine("Order Number: 1");
+        order1.DisplayResults();
+        Console.WriteLine();
+
+        // Order 2
+        Address address2 = new Address("352 St", "DogFur", "CA 85001", "USA");
+        Customer customer2 = new Customer("Blake A. Stott", address2);
+        List<Product> productsList2 = new List<Product>();
+
+        Product p2001 = new Product("Highlighters", "HLSET", 4.99, 3);
+        Product p2002 = new Product("Playing Cards", "IC100", 1.75, 2);
+        Product p2003 = new Product("Calculator", "SCC1000", 12.50, 1);
+
+        productsList2.Add(p2001);
+        productsList2.Add(p2002);
+        productsList2.Add(p2003);
+
+        Order order2 = new Order(productsList2, customer2);
+
+        Console.WriteLine("Order Number: 2");
+        order2.DisplayResults();
+        Console.WriteLine();
+
+        // Order 3
+        Address address3 = new Address("2005 W 250 S", "Salt Lake", "UT 84104", "USA");
+        Customer customer3 = new Customer("Bob H. Mostacero", address3);
+        List<Product> productsList3 = new List<Product>();
+
+        Product p3001 = new Product("Suit Case", "PC100", 80.99, 2);
+        Product p3002 = new Product("Highlighters", "HLSET", 4.99, 4);
+
+        productsList3.Add(p3001);
+        productsList3.Add(p3002);
+
+        Order order3 = new Order(productsList3, customer3);
+
+        Console.WriteLine("Order Number: 3");
+        order3.DisplayResults();
+        Console.WriteLine();
     }
 }
